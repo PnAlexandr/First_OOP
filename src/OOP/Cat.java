77bef7e03799985cf250227f3acf596898378028
pnalexandr <pnalexandr@gmail.com>
2016-12-26 13:36:13 +0300

    Начало Урока

--- src/OOP/Cat.java
+++ src/OOP/Cat.java
@@ -4,4 +4,27 @@ package OOP;
  * Created by Pavlovan on 26.12.2016.
  */
 public class Cat {
+    static class Cet{
+        String name;
+        String color;
+        int age;
+        String[] eatenMices =new String[5];
+    }
+
+    public static void main(String[] args) {
+        Cet c0;
+        c0 = new Cet();
+        c0.age=5;
+        Cet c1 =new Cet();
+        c1.age=1;
+        c1.name="Barsik";
+        c1.color="Black";
+
+
+
+        System.out.println(c0.age);
+        System.out.println(c1.age);
+        System.out.println();
+
+    }
 }
--- src/OOP/Human.java
+++ src/OOP/Human.java
@@ -4,4 +4,11 @@ package OOP;
  * Created by Pavlovan on 26.12.2016.
  */
 public class Human {
+    static class Employee{
+        String [] FIO = new String[3];
+        String position;
+        String tel;
+        float salary;
+        int age ;
+    }
 }
