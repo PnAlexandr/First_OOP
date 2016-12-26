79cdff18454fe23b42c3d7a054b723b8d6310352
pnalexandr <pnalexandr@gmail.com>
2016-12-26 15:11:33 +0300

    Очередные правки Методы первые

--- src/OOP/Cat.java
+++ src/OOP/Cat.java
@@ -1,5 +1,7 @@
 package OOP;
 
+import java.util.Arrays;
+
 /**
  * Created by Pavlovan on 26.12.2016.
  */
@@ -9,6 +11,15 @@ public class Cat {
         String color;
         int age;
         String[] eatenMices =new String[5];
+
+        public  void meaw(){
+            System.out.println("Meaw!");
+        }
+        void  jump(){
+            if(age<5) System.out.println("I jump");
+            else
+                System.out.println("I'm an abut go home");
+        }
     }
 
     public static void main(String[] args) {
@@ -19,12 +30,14 @@ public class Cat {
         c1.age=1;
         c1.name="Barsik";
         c1.color="Black";
-
+        c1.meaw();
+        c1.jump();
+        c0.jump();
 
 
         System.out.println(c0.age);
         System.out.println(c1.age);
-        System.out.println();
+        System.out.println(Arrays.toString(c1.eatenMices));
 
     }
 }
--- src/OOP/Human.java
+++ src/OOP/Human.java
@@ -4,11 +4,18 @@ package OOP;
  * Created by Pavlovan on 26.12.2016.
  */
 public class Human {
-    static class Employee{
+    static private class Employee{
         String [] FIO = new String[3];
         String position;
         String tel;
         float salary;
         int age ;
+        public void name{
+            System.out.println(FIO);
+            System.out.println(position);
+            System.out.println(tel);
+            System.out.println(salary);
+            System.out.println(age);
+        }
     }
 }
